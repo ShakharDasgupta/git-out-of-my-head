20425a7740309cd72f9ef13a541ca8f2f66077a9
Christopher Wells <cwellsny@nycap.rr.com>
2016-03-05 16:56:29 -0500

    Add Data Analyzer
    Add a class to analyze the Muse data.

--- src/com/dotgitignore/gitoutofmyhead/Main.java
+++ src/com/dotgitignore/gitoutofmyhead/Main.java
@@ -27,7 +27,8 @@ public class Main {
      */
     public static void main(String[] args) {
         int port = 5000;
-        MuseServer server = new MuseServer(port);
+        int bufferSize = 5;
+        DataAnalyzer da = new DataAnalyzer(bufferSize, port);
     }
 
 }
